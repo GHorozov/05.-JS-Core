6e595c4880e898e2d7b0bd7ef57b3515cc4908da
GHorozov <georgihorozov64@gmail.com>
2018-05-16 15:49:27 +0300

    add tasks

--- 03. JS Aplications/06. Exams/02. 15 april 2018/02. 15 april 2018- with jQuery/scripts/startApp.js
+++
@@ -1,9 +0,0 @@
-function startApp() {
-    showMenuLinks();
-    if (sessionStorage.getItem('authToken') === null) {
-        showView('welcome-section');
-    }else{
-       showUserHome();
-    }
-    attachAllEvents();
-}
